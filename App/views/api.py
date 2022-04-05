d2c254ea8ef1c8bee42a9ae8d744fc5f0217918c
keonkhelawan <k-khelawan@hotmail.com>
2022-04-05 00:07:45 +0000

    keon's changes

--- App/views/api.py
+++ App/views/api.py
@@ -1,7 +1,10 @@
 from flask import Blueprint, redirect, render_template, request, send_from_directory
+from App.database import db
+from App.models import Job
 
 api_views = Blueprint('api_views', __name__, template_folder='../templates')
 
 @api_views.route('/', methods=['GET'])
 def get_api_docs():
-    return render_template('index.html')
+    list_of_jobs = Job.query.all()
+    return render_template('index.html', jobData=list_of_jobs)
